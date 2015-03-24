b4618a3ba8d49e14388b6241ab20089a71cae94e
d-theus <slma0x02@gmail.com>
2015-03-24 21:13:16 +0300

    some design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def inline_svg(name, path='app/assets/images')
+    name += '.svg' unless name.ends_with?('.svg')
+    raw File.open(File.join(path,name)).read
+  end
 end
